9a18c1382366e6daa8495346a88f8d0f98f94867
Chris Bowers <pcbowers@liberty.edu>
2023-06-04 15:00:23 -0400

    Add custom deno lspconfig support

--- dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = {
+local default_servers = {
   "html",
   "cssls",
   "angularls",
@@ -15,10 +15,9 @@ local servers = {
   "gradle_ls",
   "rust_analyzer",
   "jdtls",
-  "denols"
 }
 
-for _, lsp in ipairs(servers) do
+for _, lsp in ipairs(default_servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
@@ -43,6 +42,19 @@ lspconfig.lua_ls.setup {
       diagnostics = {
         globals = { "vim" },
       },
+    },
+  },
+}
+
+lspconfig.denols.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    deno = {
+      enable = true,
+      lint = true,
+      unstable = true,
+      importMap = "./import_map.json",
     },
   },
 }
